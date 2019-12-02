628372b92f8af11dcdb287d8ecb53bf43f2a7ebe
ferueda <fe.rueda@outlook.com>
2019-12-01 21:24:06 -0300

    todos app project

---
+++ js-bootcamp/projects/todos.js
@@ -0,0 +1,40 @@
+const todo = [
+  {
+    name: 'Order cat food',
+    completed: false
+  },
+  {
+    name: 'Clean kitchen',
+    completed: false
+  },
+  {
+    name: 'Buy food',
+    completed: true
+  },
+  {
+    name: 'Do work',
+    completed: false
+  },
+  {
+    name: 'Excersise',
+    completed: true
+  },
+  {
+    name: 'Brush teeth',
+    completed: false
+  }
+];
+
+const deleteTodo = function(notes, name) {
+  const index = notes.findIndex(todo => todo.name.toLowerCase() === name.toLowerCase());
+  if (index !== -1) return notes.splice(index, 1);
+  else return "Todo doesn't exist";
+};
+
+const addTodo = function(notes, name, status) {
+  const newTodo = {
+    name: name,
+    completed: status
+  };
+  return notes.push(newTodo);
+};
