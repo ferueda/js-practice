fc5ce3777a972fbeed52a2406c8754391f4448bf
ferueda <fe.rueda@outlook.com>
2019-12-03 08:11:36 -0300

    todo-app initial commit

---
+++ js-bootcamp/todo-app/scripts.js
@@ -0,0 +1,29 @@
+const todo = [
+  {
+    name: 'Order cat food',
+    completed: false
+  },
+  {
+    name: 'Clean kitchen',
+    completed: false
+  },
+  {
+    name: 'Buy food',
+    completed: true
+  },
+  {
+    name: 'Do work',
+    completed: false
+  },
+  {
+    name: 'Excersise',
+    completed: true
+  },
+  {
+    name: 'Brush teeth',
+    completed: false
+  }
+];
+
+//You have 2 todos left. (p element).
+//Add a p for each todo above.
