a3ab0add1342f75313213fd75bbd6c0a548672b1
ferueda <fe.rueda@outlook.com>
2019-12-03 07:16:27 -0300

    added income and expense methods

--- js-bootcamp/projects/expense-tracker.js
+++ js-bootcamp/projects/expense-tracker.js
@@ -1,6 +1,13 @@
 const account = {
   name: 'Felipe Rueda',
+  income: [],
   expenses: [],
+  addIncome: function(description, amount) {
+    this.income.push({
+      description: description,
+      amount: amount
+    });
+  },
   addExpense: function(description, amount) {
     this.expenses.push({
       description: description,
@@ -8,11 +15,17 @@ const account = {
     });
   },
   getAccountSummary: function() {
-    const sum = this.expenses.reduce((acc, expense) => acc + expense.amount, 0);
-    return `${this.name} has $${sum} in expenses.`;
+    const income = this.income.reduce((acc, inc) => acc + inc.amount, 0);
+    const expenses = this.expenses.reduce((acc, expense) => acc + expense.amount, 0);
+    return `${this.name}'s balance is $${income - expenses}. Has $${income} in income and $${expenses} in expenses.`;
   }
 };
 
+account.addIncome('Salary', 5000);
 account.addExpense('Rent', 1500);
 account.addExpense('Coffe', 2.5);
 console.log(account.getAccountSummary());
+
+//add income array
+//addIncome method => description, amount
+// tweak getAccountSummary => add income, expenses and return balance.
