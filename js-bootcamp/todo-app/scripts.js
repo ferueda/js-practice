76bb5ce4953e6faf6cdad918fea2352411015fe5
ferueda <fe.rueda@outlook.com>
2019-12-04 08:15:13 -0300

    added todos add and remove functionalities

--- js-bootcamp/todo-app/scripts.js
+++ js-bootcamp/todo-app/scripts.js
@@ -25,5 +25,43 @@ const todo = [
   }
 ];
 
-//You have 2 todos left. (p element).
-//Add a p for each todo above.
+const filters = {
+  searchText: ''
+};
+
+displayTodos(todo, filters);
+
+document.querySelector('#add-todo-btn').addEventListener('click', function() {
+  const nt = document.querySelector('#add-todo-input');
+  if (nt.value) {
+    let p = document.createElement('p');
+    p.textContent = `${nt.value} (${false})`;
+    document.querySelector('body').appendChild(p);
+  }
+});
+
+document.querySelector('#remove-todo-btn').addEventListener('click', function() {
+  todo.splice(0);
+  displayTodos(todo, filters);
+});
+
+document.querySelector('#search-todo-input').addEventListener('input', function() {
+  filters.searchText = this.value;
+  displayTodos(todo, filters);
+});
+
+function displayTodos(todos, filter) {
+  const filteredNotes = todos.filter(todo => todo.name.toLowerCase().includes(filter.searchText.toLowerCase()));
+
+  document.querySelector('#todos-container').innerHTML = '';
+
+  const todosLeft = document.createElement('h2');
+  todosLeft.textContent = `You have ${filteredNotes.length} todos left.`;
+  document.querySelector('#todos-container').appendChild(todosLeft);
+
+  filteredNotes.forEach(todo => {
+    let p = document.createElement('p');
+    p.textContent = todo.name;
+    document.querySelector('#todos-container').appendChild(p);
+  });
+}
