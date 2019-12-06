66a995f2ebe82afa1b42f687b0e0217c4c7cd9dc
ferueda <fe.rueda@outlook.com>
2019-12-06 07:28:45 -0300

    added localStorage func

--- js-bootcamp/todo-app/scripts.js
+++ js-bootcamp/todo-app/scripts.js
@@ -1,34 +1,12 @@
-const todo = [
-  {
-    name: 'Order cat food',
-    completed: false
-  },
-  {
-    name: 'Clean kitchen',
-    completed: false
-  },
-  {
-    name: 'Buy food',
-    completed: true
-  },
-  {
-    name: 'Do work',
-    completed: false
-  },
-  {
-    name: 'Excersise',
-    completed: true
-  },
-  {
-    name: 'Brush teeth',
-    completed: false
-  }
-];
+let todos = [];
 
 const filters = {
   searchText: '',
   hideCompleted: false
 };
+
+const todosJSON = localStorage.getItem('todos');
+if (todosJSON) todos = JSON.parse(todosJSON);
 
 //render todos
 const displayTodos = function(todos, filter) {
@@ -55,26 +33,27 @@ const displayTodos = function(todos, filter) {
 //search todos and add to filter object
 document.querySelector('#search-todo-input').addEventListener('input', function() {
   filters.searchText = this.value;
-  displayTodos(todo, filters);
+  displayTodos(todos, filters);
 });
 
 //hide completed todos
 document.querySelector('#completed-check').addEventListener('change', function(e) {
   filters.hideCompleted = e.target.checked;
-  displayTodos(todo, filters);
+  displayTodos(todos, filters);
 });
 
 document.querySelector('#add-todo-form').addEventListener('submit', function(e) {
   e.preventDefault();
   const newTodo = e.target.elements['add-todo-input'].value;
   if (newTodo) {
-    todo.push({
+    todos.push({
       name: newTodo,
       completed: false
     });
+    localStorage.setItem('todos', JSON.stringify(todos));
+    displayTodos(todos, filters);
     e.target.elements['add-todo-input'].value = '';
-    displayTodos(todo, filters);
   }
 });
 
-displayTodos(todo, filters);
+displayTodos(todos, filters);
