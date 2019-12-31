bfcaa043dfce5c68cda0adac7c5e8cfe3c77c4ff
ferueda <fe.rueda@outlook.com>
2019-12-30 22:49:48 -0300

    refactored functions

--- js-bootcamp/notes-app/script.js
+++ js-bootcamp/notes-app/script.js
@@ -5,12 +5,12 @@ const filters = {
   sortBy: 'byEdited'
 };
 
-document.querySelector('#search-input').addEventListener('input', function(e) {
+document.querySelector('#search-input').addEventListener('input', e => {
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
 
-document.querySelector('#create-note-btn').addEventListener('click', function() {
+document.querySelector('#create-note-btn').addEventListener('click', () => {
   const id = uuidv4();
   const timestamp = moment().valueOf();
 
@@ -25,12 +25,12 @@ document.querySelector('#create-note-btn').addEventListener('click', function() 
   location.assign(`edit.html#${id}`);
 });
 
-document.querySelector('#sort-notes').addEventListener('change', function(e) {
+document.querySelector('#sort-notes').addEventListener('change', e => {
   filters.sortBy = e.target.value;
   renderNotes(notes, filters);
 });
 
-window.addEventListener('storage', function(e) {
+window.addEventListener('storage', e => {
   if (e.key === 'notes') {
     notes = JSON.parse(e.newValue);
     renderNotes(notes, filters);
